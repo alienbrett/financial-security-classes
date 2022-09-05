c509a0cd89afb15204a0f4373e6fbfe28a2577d5
Brett Graves <alienbrett648@gmail.com>
2022-09-05 15:39:53 +0000

    added americans + test

--- finsec/options.py
+++ finsec/options.py
@@ -126,3 +126,111 @@ def European (
         website             = website,
         issuer              = None,
     )
+
+
+
+
+
+def American (
+        # Will link against this underlier
+        underlying_security : Security,
+
+        multiplier          : Multiplier,
+
+        expiry_time_of_day  : ExpiryTimeOfDay,
+
+        strike              : CurrencyQty,
+        
+        ## Callput should be in ('call','put')
+        callput             : str,
+
+        expiry_date         : datetime.date,
+        expiry_datetime     : Optional[datetime.datetime]   = None,
+
+        ### If underlier is an index, then settlement inferred as cash-settled
+        settlement_type     : Optional[SettlementType]      = None,
+
+        ### If none, this is set to UNKNOWN
+        expiry_series_type  : Optional[ExpirySeriesType]    = None,
+
+        primary_exc         : Optional[Exchange]            = None,
+        website             : Optional[str]                 = None,
+        identifiers         : List[SecurityIdentifier]      = [],
+
+        gsid                : GSID                          = None,
+
+        ### Currency will be used, if it cannot be inferred from underlying
+        currency            : Union[Security,SecurityReference,None] = None,
+    ) -> Option:
+    description = None
+
+    good_callput = callput.strip().lower()
+    if good_callput == 'call':
+        flavor = OptionFlavor.CALL
+    elif good_callput == 'put':
+        flavor = OptionFlavor.PUT
+    else:
+        raise ValueError('Unknown callput type: {0}'.format(callput))
+
+    if is_physical_settlement_available( underlying_security ):
+        if settlement_type is None:
+            raise InvalidSettlementTypeException('Must specify settlement type, since underlying permits physical delivery')
+    else:
+
+        if settlement_type == SettlementType.PHYSICAL:
+            raise InvalidSettlementTypeException('Cannot physically-settle a derived index')
+        else:
+            settlement_type = SettlementType.CASH
+        
+    if expiry_series_type is None:
+        expiry_series_type = ExpirySeriesType.UNKNOWN
+
+    underlying_ref = create_reference_from_security( underlying_security )
+
+    if isinstance(expiry_date, str):
+        expiry_date = datetime.datetime.strptime(expiry_date, '%Y-%m-%d').date()
+    if expiry_datetime is not None and expiry_date is None:
+        expiry_date = expiry_datetime.date()
+
+    occ_ticker = option_format(
+        symbol      = underlying_ref.ticker,
+        exp_date    = expiry_date,
+        flavor      = callput,
+        strike      = strike,
+    )
+
+    if currency is None:
+        if underlying_security.denominated_ccy is None:
+            raise UnknownDenominatedCurrencyException('Must declare denominated currency, if not supplied by underlier')
+        else:
+            currency = underlying_security.denominated_ccy
+    elif isinstance(currency, Security):
+            currency = create_reference_from_security( currency )
+
+    return Option(
+        gsid                = gsid,
+        ticker              = occ_ticker,
+        strike              = strike,
+        option_flavor       = flavor,
+
+        security_type       = SecurityType.DERIVATIVE,
+        security_subtype    = SecuritySubtype.EQUITY_OPTION,
+
+        option_exercise     = OptionExerciseStyle.AMERICAN,
+        settlement_type     = settlement_type,
+        expiry_date         = expiry_date,
+        expiry_datetime     = expiry_datetime,
+        expiry_time_of_day  = expiry_time_of_day,
+        underlying          = underlying_ref,
+
+        multiplier          = multiplier,
+        denominated_ccy     = currency,
+        expiry_series_type  = expiry_series_type,
+
+        primary_exchange    = primary_exc,
+
+        description         = description,
+        identifiers         = identifiers,
+        website             = website,
+        issuer              = None,
+    )
--- tests/test_options.py
+++ tests/test_options.py
@@ -58,6 +58,7 @@ class TestOptionConstructor(unittest.TestCase):
         self.assertEqual(option.security_type, fs.SecurityType.DERIVATIVE)
         self.assertEqual(option.security_subtype, fs.SecuritySubtype.EQUITY_OPTION)
         self.assertEqual(option.option_flavor, fs.OptionFlavor.CALL)
+        self.assertEqual(option.option_exercise, fs.OptionExerciseStyle.EUROPEAN)
 
 
         self.assertEqual(option.primary_exchange, fs.Exchange.CBOE)
@@ -110,6 +111,7 @@ class TestOptionConstructor(unittest.TestCase):
         self.assertEqual(option.security_type, fs.SecurityType.DERIVATIVE)
         self.assertEqual(option.security_subtype, fs.SecuritySubtype.EQUITY_OPTION)
         self.assertEqual(option.option_flavor, fs.OptionFlavor.CALL)
+        self.assertEqual(option.option_exercise, fs.OptionExerciseStyle.EUROPEAN)
 
         self.assertEqual(option.multiplier, 100,)
 
@@ -186,6 +188,7 @@ class TestOptionConstructor(unittest.TestCase):
         self.assertEqual(option.security_type, fs.SecurityType.DERIVATIVE)
         self.assertEqual(option.security_subtype, fs.SecuritySubtype.EQUITY_OPTION)
         self.assertEqual(option.option_flavor, fs.OptionFlavor.PUT)
+        self.assertEqual(option.option_exercise, fs.OptionExerciseStyle.EUROPEAN)
 
         self.assertEqual(option.primary_exchange, fs.Exchange.CBOE)
         self.assertEqual(option.expiry_date, datetime.date(2022,9,16))
@@ -236,6 +239,7 @@ class TestOptionConstructor(unittest.TestCase):
         self.assertEqual(option.security_type, fs.SecurityType.DERIVATIVE)
         self.assertEqual(option.security_subtype, fs.SecuritySubtype.EQUITY_OPTION)
         self.assertEqual(option.option_flavor, fs.OptionFlavor.PUT)
+        self.assertEqual(option.option_exercise, fs.OptionExerciseStyle.EUROPEAN)
 
         self.assertEqual(option.multiplier, 100,)
 
@@ -260,6 +264,273 @@ class TestOptionConstructor(unittest.TestCase):
     def test_create_european_put_3(self,):
 
         option = fs.European(
+            gsid                = fs.GSID(120),
+            underlying_security = self.spx,
+
+            ## I understand this dates the test, using SPX 4,000 calls!
+            ## Or I hope this dates the test...
+            callput             = 'put',
+            strike              = 4000,
+
+            expiry_date         = '2022-09-16',
+            primary_exc         = fs.Exchange.CBOE,
+
+            expiry_time_of_day  = fs.ExpiryTimeOfDay.OPEN,
+
+            multiplier          = 100.0,
+
+            identifiers         = [
+                fs.FIGI('234567'),
+            ],
+
+            #### THis line should generate an exception
+            settlement_type     = fs.SettlementType.PHYSICAL,
+
+        )
+    
+
+
+
+
+
+    def test_create_american_call_1(self,):
+
+        option = fs.American(
+            gsid                = fs.GSID(120),
+            underlying_security = self.spx,
+
+            ## I understand this dates the test, using SPX 4,000 calls!
+            ## Or I hope this dates the test...
+            callput             = 'call',
+            strike              = 4000,
+
+            expiry_date         = datetime.date(2022,9,16),
+            primary_exc         = fs.Exchange.CBOE,
+
+            expiry_time_of_day  = fs.ExpiryTimeOfDay.OPEN,
+
+            multiplier          = 100.0,
+
+            identifiers         = [
+                fs.FIGI('234567'),
+            ],
+
+            #### This should be implied, since underlyer doesn't permit physical delivery
+            # settlement_type     = fs.SettlementType.CASH,
+            #### Without this argument, this should be set to UNKNOWN
+            # expiry_series_type  = fs.ExpirySeriesType.MONTHLY,
+
+        )
+
+        self.assertEqual(option.security_type, fs.SecurityType.DERIVATIVE)
+        self.assertEqual(option.security_subtype, fs.SecuritySubtype.EQUITY_OPTION)
+        self.assertEqual(option.option_flavor, fs.OptionFlavor.CALL)
+        self.assertEqual(option.option_exercise, fs.OptionExerciseStyle.AMERICAN)
+
+
+        self.assertEqual(option.primary_exchange, fs.Exchange.CBOE)
+        self.assertEqual(option.expiry_date, datetime.date(2022,9,16))
+        
+        # ## THese should berred without needing to be explicitly set
+        self.assertEqual(option.expiry_series_type, fs.ExpirySeriesType.UNKNOWN)
+        self.assertEqual(option.settlement_type, fs.SettlementType.CASH)
+
+        self.assertEqual(option.ticker, 'SPX220916C04000000')
+        self.assertEqual(option.gsid, fs.GSID(120),)
+
+        self.assertEqual(option.multiplier, 100,)
+
+        self.assertIn(fs.FIGI('234567'), option.identifiers,)
+
+        self.assertEqual(option.denominated_ccy, fs.create_reference_from_security(self.usd))
+
+
+
+    def test_create_american_call_2(self,):
+
+        option = fs.American(
+            gsid                = fs.GSID(120),
+            underlying_security = self.spx,
+
+            ## I understand this dates the test, using SPX 4,000 calls!
+            ## Or I hope this dates the test...
+            callput             = 'call',
+            strike              = 4000,
+
+            expiry_date         = '2022-09-16',
+            primary_exc         = fs.Exchange.CBOE,
+
+            expiry_time_of_day  = fs.ExpiryTimeOfDay.OPEN,
+
+            multiplier          = 100.0,
+
+            identifiers         = [
+                fs.FIGI('234567'),
+            ],
+
+            #### This should be implied, since underlyer doesn't permit physical delivery
+            # settlement_type     = fs.SettlementType.CASH,
+            #### Without this argument, this should be set to UNKNOWN
+            expiry_series_type  = fs.ExpirySeriesType.MONTHLY,
+
+        )
+
+        self.assertEqual(option.security_type, fs.SecurityType.DERIVATIVE)
+        self.assertEqual(option.security_subtype, fs.SecuritySubtype.EQUITY_OPTION)
+        self.assertEqual(option.option_flavor, fs.OptionFlavor.CALL)
+        self.assertEqual(option.option_exercise, fs.OptionExerciseStyle.AMERICAN)
+
+        self.assertEqual(option.multiplier, 100,)
+
+        self.assertEqual(option.primary_exchange, fs.Exchange.CBOE)
+        self.assertEqual(option.expiry_date, datetime.date(2022,9,16))
+        
+        self.assertEqual(option.expiry_series_type, fs.ExpirySeriesType.MONTHLY)
+        # ## THese should berred without needing to be explicitly set
+        self.assertEqual(option.settlement_type, fs.SettlementType.CASH)
+
+        self.assertEqual(option.ticker, 'SPX220916C04000000')
+        self.assertEqual(option.gsid, fs.GSID(120),)
+
+        self.assertIn(fs.FIGI('234567'), option.identifiers,)
+
+        self.assertEqual(option.denominated_ccy, fs.create_reference_from_security(self.usd))
+    
+
+
+
+    @unittest.expectedFailure
+    def test_create_american_call_3(self,):
+
+        option = fs.American(
+            gsid                = fs.GSID(120),
+            underlying_security = self.spx,
+
+            ## I understand this dates the test, using SPX 4,000 calls!
+            ## Or I hope this dates the test...
+            callput             = 'call',
+            strike              = 4000,
+
+            expiry_date         = '2022-09-16',
+            primary_exc         = fs.Exchange.CBOE,
+
+            expiry_time_of_day  = fs.ExpiryTimeOfDay.OPEN,
+
+            multiplier          = 100.0,
+
+            identifiers         = [
+                fs.FIGI('234567'),
+            ],
+
+            #### THis line should generate an exception
+            settlement_type     = fs.SettlementType.PHYSICAL,
+
+        )
+    
+
+
+    def test_create_american_put_1(self,):
+
+        option = fs.American(
+            gsid                = fs.GSID(120),
+            underlying_security = self.spx,
+
+            ## I understand this dates the test, using SPX 4,000 calls!
+            ## Or I hope this dates the test...
+            callput             = 'put',
+            strike              = 4000,
+
+            expiry_date         = datetime.date(2022,9,16),
+            primary_exc         = fs.Exchange.CBOE,
+
+            expiry_time_of_day  = fs.ExpiryTimeOfDay.OPEN,
+
+            multiplier          = 100.0,
+
+            identifiers         = [
+                fs.FIGI('234567'),
+            ],
+        )
+
+        self.assertEqual(option.security_type, fs.SecurityType.DERIVATIVE)
+        self.assertEqual(option.security_subtype, fs.SecuritySubtype.EQUITY_OPTION)
+        self.assertEqual(option.option_flavor, fs.OptionFlavor.PUT)
+        self.assertEqual(option.option_exercise, fs.OptionExerciseStyle.AMERICAN)
+
+        self.assertEqual(option.primary_exchange, fs.Exchange.CBOE)
+        self.assertEqual(option.expiry_date, datetime.date(2022,9,16))
+        
+        # ## THese should berred without needing to be explicitly set
+        self.assertEqual(option.expiry_series_type, fs.ExpirySeriesType.UNKNOWN)
+        self.assertEqual(option.settlement_type, fs.SettlementType.CASH)
+
+        self.assertEqual(option.ticker, 'SPX220916P04000000')
+        self.assertEqual(option.gsid, fs.GSID(120),)
+
+        self.assertEqual(option.multiplier, 100,)
+
+        self.assertIn(fs.FIGI('234567'), option.identifiers,)
+
+        self.assertEqual(option.denominated_ccy, fs.create_reference_from_security(self.usd))
+
+
+
+    def test_create_american_put_2(self,):
+
+        option = fs.American(
+            gsid                = fs.GSID(120),
+            underlying_security = self.spx,
+
+            ## I understand this dates the test, using SPX 4,000 calls!
+            ## Or I hope this dates the test...
+            callput             = 'put',
+            strike              = 4000,
+
+            expiry_date         = '2022-09-16',
+            primary_exc         = fs.Exchange.CBOE,
+
+            expiry_time_of_day  = fs.ExpiryTimeOfDay.OPEN,
+
+            multiplier          = 100.0,
+
+            identifiers         = [
+                fs.FIGI('234567'),
+            ],
+
+            #### This should be implied, since underlyer doesn't permit physical delivery
+            # settlement_type     = fs.SettlementType.CASH,
+            #### Without this argument, this should be set to UNKNOWN
+            expiry_series_type  = fs.ExpirySeriesType.MONTHLY,
+        )
+
+        self.assertEqual(option.security_type, fs.SecurityType.DERIVATIVE)
+        self.assertEqual(option.security_subtype, fs.SecuritySubtype.EQUITY_OPTION)
+        self.assertEqual(option.option_flavor, fs.OptionFlavor.PUT)
+        self.assertEqual(option.option_exercise, fs.OptionExerciseStyle.AMERICAN)
+
+        self.assertEqual(option.multiplier, 100,)
+
+        self.assertEqual(option.primary_exchange, fs.Exchange.CBOE)
+        self.assertEqual(option.expiry_date, datetime.date(2022,9,16))
+        
+        self.assertEqual(option.expiry_series_type, fs.ExpirySeriesType.MONTHLY)
+        # ## THese should berred without needing to be explicitly set
+        self.assertEqual(option.settlement_type, fs.SettlementType.CASH)
+
+        self.assertEqual(option.ticker, 'SPX220916P04000000')
+        self.assertEqual(option.gsid, fs.GSID(120),)
+
+        self.assertIn(fs.FIGI('234567'), option.identifiers,)
+
+        self.assertEqual(option.denominated_ccy, fs.create_reference_from_security(self.usd))
+    
+
+
+
+    @unittest.expectedFailure
+    def test_create_american_put_3(self,):
+
+        option = fs.American(
             gsid                = fs.GSID(120),
             underlying_security = self.spx,
 
