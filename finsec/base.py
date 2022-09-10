c81a5482b1fed9a7941730ab4a778c77d6f0e3e8
alienbrett <alienbrett648@gmail.com>
2022-09-10 15:04:22 -0400

    Added optional strawberry + pydantic support

--- finsec/base.py
+++ finsec/base.py
@@ -1,34 +1,62 @@
+### Test optional dependencies
+# Strawberry #
+try:
+    import strawberry
+    strawberry_enabled = True
+except:
+    strawberry_enabled = False
+# print('Strawberry enabled: {0}'.format(str(strawberry_enabled)))
+
+# Pydantic #
+try:
+    from pydantic.dataclasses import dataclass
+    pydantic_enabled = True
+except:
+    from dataclasses import dataclass
+    pydantic_enabled = False
+# print('pydantic enabled: {0}'.format(str(pydantic_enabled)))
+
+
+####################
 import typing
 import dataclasses
 import datetime
-
 from dataclasses_json import dataclass_json
-
 
 from .enums         import *
 from .exceptions    import *
 from .exchanges     import *
 from .config        import *
 
+
 placeholder = lambda: dataclasses.field(default=False)
 
 
+
 @dataclass_json
-@dataclasses.dataclass
+@dataclass
 class SecurityIdentifier:
     id_type : SecurityIdentifierType
     value   : str
 
+if strawberry_enabled:
+    SecurityIdentifier = strawberry.type()(SecurityIdentifier)
+
+
 
 @dataclass_json
-@dataclasses.dataclass
+@dataclass
 class SecurityReference:
     gsid                : GSID
     ticker              : Ticker
 
+if strawberry_enabled:
+    SecurityReference = strawberry.type()(SecurityReference)
+
+
 
 @dataclass_json
-@dataclasses.dataclass
+@dataclass
 class Security:
     ### Application-level globally unique security id
     # Burden is on user to assign these in unique way,
@@ -59,11 +87,13 @@ class Security:
     def __post_init__(self,):
         self.ticker = self.ticker.strip().upper()
 
+if strawberry_enabled:
+    Security = strawberry.type()(Security)
 
 
 
 @dataclass_json
-@dataclasses.dataclass
+@dataclass
 class Derivative(Security):
 
     ### Underlying that determines settlement of the contract
@@ -81,22 +111,31 @@ class Derivative(Security):
     ####### The multiplier vs underlier
     multiplier          : Multiplier = placeholder()
 
+if strawberry_enabled:
+    Derivative = strawberry.type()(Derivative)
 
 
 @dataclass_json
-@dataclasses.dataclass
+@dataclass
 class Future(Derivative):
     ####### The minimum tick sizerement
     tick_size           : CurrencyQty = placeholder()
 
+if strawberry_enabled:
+    Future = strawberry.type()(Future)
+
 
 @dataclass_json
-@dataclasses.dataclass
+@dataclass
 class Option(Derivative):
 
     option_flavor   : OptionFlavor = placeholder()
     option_exercise : OptionExerciseStyle = placeholder()
 
     strike          : CurrencyQty = placeholder()
+
+if strawberry_enabled:
+    Option = strawberry.type()(Option)
+
 
 
