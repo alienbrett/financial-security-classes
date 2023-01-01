518c7abeff1de79f7933374c4fb9d4bc8ecd7fcf
Brett Graves <alienbrett648@gmail.com>
2023-01-01 17:32:52 +0000

    black'd some more

--- finsec/exchanges.py
+++ finsec/exchanges.py
@@ -1,28 +1,19 @@
-# try:
-#     import strawberry
-#     strawberry_enabled = True
-# except:
-#     strawberry_enabled = False
-
 import enum
 
 
 class Exchange(str, enum.Enum):
-    UNKNOWN     = 'UNK'
+    UNKNOWN = "UNK"
 
-    ### USA
-    NYSE        = 'NYS'
-    NASDAQ      = 'NSQ'
-    ARCA        = 'ARC'
-    BATS        = 'BAT'
-    IEX         = 'IEX'
-    PHLX        = 'PHX'
-    AMEX        = 'AMX'
-    CME         = 'CME'
-    CBOE        = 'CBO'
+    # USA
+    NYSE = "NYS"
+    NASDAQ = "NSQ"
+    ARCA = "ARC"
+    BATS = "BAT"
+    IEX = "IEX"
+    PHLX = "PHX"
+    AMEX = "AMX"
+    CME = "CME"
+    CBOE = "CBO"
 
-
-    ### Canadian
-    TORONTO     = 'TSX'
-# if strawberry_enabled:
-#     Exchange = strawberry.type()(Exchange)
+    # Canadian
+    TORONTO = "TSX"
