06189e209dad3c2c4b3fb58b9df556c229882cb7
Brett Graves <alienbrett648@gmail.com>
2023-01-01 18:01:10 +0000

    more blacking

--- finsec/occ_symbology.py
+++ finsec/occ_symbology.py
@@ -1,16 +1,22 @@
-import typing
-
 import datetime
 import math
+import typing
 
-__all__ = ['OCCSymbol','option_format','option_strike','option_flavor','option_maturity','option_symbol']
+__all__ = [
+    "OCCSymbol",
+    "option_format",
+    "option_strike",
+    "option_flavor",
+    "option_maturity",
+    "option_symbol",
+]
 
-OCCSymbol = typing.NewType('OCCSymbol',str)
+OCCSymbol = typing.NewType("OCCSymbol", str)
 
 
-fmt1 = '%Y-%m-%d'
+fmt1 = "%Y-%m-%d"
 fmt2 = "%y%m%d"
-    
+
 
 def format_strike(strike):
     x = str(math.floor(float(strike) * 1000))
@@ -18,12 +24,13 @@ def format_strike(strike):
 
 
 def option_format(
-        symbol      : str,
-        exp_date    : typing.Union[datetime.date, datetime.datetime, str],
-        strike      : typing.Union[float,int],
-        flavor      : typing.Literal['call','put'],
-    ) -> OCCSymbol:
+    symbol: str,
+    exp_date: typing.Union[datetime.date, datetime.datetime, str],
+    strike: typing.Union[float, int],
+    flavor: typing.Literal["call", "put"],
+) -> OCCSymbol:
     """Returns the OCC standardized option name.
+
     Args:
             symbol: the underlying symbol, case insensitive
             exp_date: date of expiration, in string-form.
@@ -31,58 +38,44 @@ def option_format(
             flavor: 'C' or 'call' or the like, for call, otherwise 'p' or 'Put' for put
     Returns:
             OCC string, like 'IBM201231C00301000'
-    .. code-block:: python
-            # Construct the option's OCC symbol
-            >>> ibm_call = option_format(
-                    exp_date = '2020-12-31',
-                    symbol = 'IBM', # case insensitive
-                    flavor = 'call',
-                    strike = 301
-            )
-            >>> ibm_call
-            'IBM201231C00301000'
     """
-
     if isinstance(exp_date, str):
-        exp_date = datetime.datetime.strptime(exp_date,fmt1).date()
-    
-    assert( isinstance(exp_date, datetime.date) or isinstance(exp_date, datetime.datetime))
+        exp_date = datetime.datetime.strptime(exp_date, fmt1).date()
 
-    # direction into C or P
-    if flavor == 'call':
-        d_char = 'C'
-    elif flavor == 'put':
-        d_char = 'P'
+    assert isinstance(exp_date, datetime.date) or isinstance(
+        exp_date, datetime.datetime
+    )
+
+    if flavor == "call":
+        d_char = "C"
+    elif flavor == "put":
+        d_char = "P"
 
     symbol = str(symbol).strip().upper()
 
-    # Assemble
-    return '{symbol}{maturity}{d}{strike}'.format(
-        symbol      = symbol,
-        maturity    = exp_date.strftime(fmt2),
-        d           =  d_char,
-        strike      = format_strike(strike),
+    return "{symbol}{maturity}{d}{strike}".format(
+        symbol=symbol,
+        maturity=exp_date.strftime(fmt2),
+        d=d_char,
+        strike=format_strike(strike),
     )
 
 
-def option_strike(name : OCCSymbol):
-    """Pull apart an OCC standardized option name and
-    retreive the strike price, in integer form"""
+def option_strike(name: OCCSymbol):
+    """Returns option strike from OCC standardized option name."""
     return int(name[-8:]) / 1000.0
 
 
-def option_maturity(name : OCCSymbol) -> datetime.date:
-    """Given OCC standardized option name,
-    return the date of maturity"""
+def option_maturity(name: OCCSymbol) -> datetime.date:
+    """Returns option maturity date from OCC standardized option name."""
     return datetime.datetime.strptime(name[-15:-9], fmt2).strftime(fmt1)
 
 
-def option_flavor(name : OCCSymbol) -> typing.Literal['call','put']:
-    """Given OCC standardized option name,
-    return whether its a call or a put"""
+def option_flavor(name: OCCSymbol) -> typing.Literal["call", "put"]:
+    """Returns option flavor ticker from OCC standardized option name."""
     return "call" if name.upper()[-9] == "C" else "put"
 
 
-def option_symbol(name : OCCSymbol) -> str:
-    """Given OCC standardized option name, return option ticker"""
+def option_symbol(name: OCCSymbol) -> str:
+    """Returns option underlying ticker from OCC standardized option name."""
     return name[:-15]
