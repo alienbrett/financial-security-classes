5c0d43a8c2ab23cec402b37bf1fe4d871e421bb1
Brett Graves <alienbrett648@gmail.com>
2023-01-01 22:51:31 +0000

    updated doc build action version

--- setup.py
+++ setup.py
@@ -158,7 +158,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
