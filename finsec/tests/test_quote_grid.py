79b86ddc6c075a7949e40cd5ebf4f4beceff899f
Brett Graves <alienbrett648@gmail.com>
2023-01-02 17:40:03 +0000

    added quote coverage tests and fixed some bugs

--- finsec/tests/test_quote_grid.py
+++ finsec/tests/test_quote_grid.py
@@ -180,6 +180,7 @@ class TestQuoteGrids(BaseTestCase):
         )
 
         self.assertEqual(q1 + q2, q3)
+        self.assertEqual(q3 - q1, q2)
         self.assertEqual(q1 * 2, q1 + q1)
         self.assertEqual(q3 * 2, q3 + q3)
 
